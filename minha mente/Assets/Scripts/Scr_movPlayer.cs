f0d253e24434feb296b4ab0ccc9a000aa039827b
Loop <loopzwx@gmail.com>
2025-05-07 13:29:59 -0300

    modifc. lv1 aprimoramento scrMovPlayer

--- minha mente/Assets/Scripts/Scr_movPlayer.cs
+++ minha mente/Assets/Scripts/Scr_movPlayer.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class Scr_movPlayer : MonoBehaviour
 {
-    public float speed = 5.0f; // Velocidade do player
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 100f;
 
     // Update is called once per frame
     void Update() {
         // Obtém a entrada do teclado
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput).normalized * moveSpeed * Time.deltaTime;
+        transform.Translate(movement);
 
-        // Calcula o vetor de movimento
-        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput).normalized;
-
-        // Aplica a movimentação
-        transform.Translate(movement * speed * Time.deltaTime);
-
+        // Rotação
+        float rotationInput = Input.GetAxis("Mouse X"); // Ou joystick
+        transform.Rotate(0, rotationInput * rotationSpeed * Time.deltaTime, 0);
     }
 }
